b14fae3c01283d46f7d08774f5b8a5fed9924ee1
Lalithsai853 <56562323+Lalithsai853@users.noreply.github.com>
2020-06-14 23:49:22 -0700

    Add files via upload

---
+++ unit5/examples/Unit5_For_Loop.py
@@ -0,0 +1,28 @@
+#Example1
+for letter in "Python":    
+   print("The current letter is", letter)
+
+#Example2
+
+programming_languages = ["Python", "Java", "C++", "C"]
+for language in programming_languages:        
+   print (language)
+
+#Example3
+
+programming_languages = ["Python", "Java", "C++", "C"]
+for language in programming_languages:        
+   print (language)
+
+else:
+   print("Finished looping through the list!")
+
+#Example3 
+
+
+
+sum = 0
+for num in range(1,101):
+    sum = sum + num
+
+print("The sum of 1 until 100 is", sum)
