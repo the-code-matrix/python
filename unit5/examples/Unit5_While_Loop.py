c347759f42942d145797b0dce48d6629a095a541
Lalithsai853 <56562323+Lalithsai853@users.noreply.github.com>
2020-06-14 23:48:44 -0700

    Add files via upload

---
+++ unit5/examples/Unit5_While_Loop.py
@@ -0,0 +1,21 @@
+#Example1 - SImple Loop
+count  =  0
+while count < 12:
+    print("Count is", count)
+    count = count + 1
+
+#Example2 - Infinite Loop
+#rand_val = 1
+#while rand_val == 1:
+    #num = input("Enter any number")
+    #print("Your number is", num) 
+
+#Example3 -While Else
+
+count = 0
+while count < 12:
+    print(count, "is less than 12")
+    count = count + 1
+
+else:
+    print(count, "is not less than 12")
